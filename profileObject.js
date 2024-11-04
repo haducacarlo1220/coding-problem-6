bb1285c070012e24edeec7f9932d4a25aaecf1be
haducacarlo1220 <ralphhaduca@gmail.com>
2024-11-04 20:19:47 +0800

    initial commit

---
+++ profileObject.js
@@ -0,0 +1,10 @@
+
+let userProfile = {
+    fullName: "Ralph Carlo H. Pano",       
+    age: 18,                    
+    favoriteNumber: 3,         
+    favoriteColor: "black"       
+};
+
+
+console.log(userProfile);
